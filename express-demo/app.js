12ca8f8af0945f71d075a82690de2bee28bc978f
Or azar <orazar1805@gmail.com>
2021-08-15 20:41:30 +0300

    joi and express

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,8 +1,19 @@
-// Express
+// joi 
+const Joi = require('joi');
 
+// Express
 const { request, response } = require('express');
 const express  = require('express');
 const app= express();
+
+app.use(express.json());
+
+
+const  courses = [ 
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+]
 
 /*
 //methods 
@@ -18,8 +29,39 @@ app.get('/', (req,res) => {
 
 app.get('/api/courses', (req,res) => {
     //res.send('Welcome to the course, have a nice day.');
-    res.send([1,2,3]);
+    res.send([courses]);
 
 })
 
-app.listen(3000, () => console.log("Listening on port 3000..."));
+app.get('/api/courses/:id', (req,res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course of this given ID was not found'); //404 - error of not found 
+    res.send("Welcome to the course number " + req.params.id); 
+});
+
+//post -  how we add a new course.
+app.post('/api/courses', (req,res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if(result.error){
+        //400 bad request
+        res.status(400).send(result.error);
+        return;
+    } 
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+// PORT 
+const port = process.env.PORT|| 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
