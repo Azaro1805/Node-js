6614e4447338793b213a776df5e458fa7b497ef9
Or azar <orazar1805@gmail.com>
2021-08-25 16:57:32 +0300

    epress adv

--- Expresss-Advanced/index.js
+++ Expresss-Advanced/index.js
@@ -1,13 +1,127 @@
+const config = require('config');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const Joi = require('joi');
+const logger = require('./logger');
+const express = require('express');
+const app = express();
 
-//underscore 
-var _  = require('underscore');
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true})); //with exteded = true with can send arrays and more complex format.
+app.use(logger);
+app.use(helmet());
+
+//Configuration
+console.log('Appliction Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+//console.log('Mail Password: ' + config.get('mail.password')); //make problem...
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+}
+
+const  courses = [ 
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
+
+
 
 /*
-require 
-first - core module
-second - File or folder
-third - node_modules(like underscore)
+//methods 
+app.get();
+app.post();
+app.put();
+app.delete();
 */
 
-ans = _.contains([1,2,3],3)
-console.log(ans)
+
+
+app.get('/', (req,res) => {
+    res.send('Hello word');
+});
+
+app.get('/api/courses', (req,res) => {
+    //res.send('Welcome to the course, have a nice day.');
+    res.send([courses]);
+
+})
+
+app.get('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course of this given ID was not found'); 
+    res.send("Welcome to the course number " + req.params.id); 
+});
+
+//post -  how we add a new course.
+app.post('/api/courses', (req,res) => {
+
+    const {error} = ValidateCourse(req.body); //{error} = result.error 
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+// PUT
+app.put('/api/courses/:id', (req,res) => {
+    //Look up the course 
+    //if not existing, return 404 - not found
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course of this given ID was not found'); 
+    
+    //validate
+    //if invalid, return 400 - bad request
+
+    //const result =  ValidateCourse(req.body);
+    const {error} = ValidateCourse(req.body); //{error} = result.error 
+    if(error) return res.status(400).send(error.details[0].message);
+    
+
+    //update course
+    course.name = req.body.name;
+    res.send(course);
+
+    //return the updated course
+});
+
+// Delete
+app.delete('/api/courses/:id', (req,res) => {
+    //Look up the course 
+    //if not existing, return 404 - not found
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course of this given ID was not found'); 
+
+    //Delete
+    
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+
+// Validate course function
+function ValidateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
+
+// PORT 
+const port = process.env.PORT|| 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
+
